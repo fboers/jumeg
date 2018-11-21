365d95ec1c909e1001b9cc99bfaef305fae68ccb
fboers <f.boers@fz-juelich.de>
2018-11-21 08:27:38 +0100

    delete all in __init__.py useless in py3

--- __init__.py
+++ __init__.py
@@ -7,21 +7,3 @@
 #          Praveen Sripad  <p.sripad@fz-juelich.de>
 #
 # License: Simplified BSD
-
-__version__ = '0.15'
-
-from . import jumeg_preprocessing
-from . import jumeg_math
-from . import jumeg_iomeg
-from . import jumeg_utils
-from . import jumeg_plot
-from . import decompose
-from . import mft
-from . import jumeg_noise_reducer
-from . import jumeg_noise_reducer_hcp
-from . import connectivity
-from .filter import jumeg_filter
-from . import jumeg_source_localize
-from . import jumeg_surrogates
-from .jumeg_utils import get_jumeg_path
-from .jumeg_suggest_bads import suggest_bads
