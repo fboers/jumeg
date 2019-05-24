87e4da3ae48351378d3d3935162845c328ab2fda
fboers <f.boers@fz-juelich.de>
2019-05-24 08:01:44 +0200

    update

--- jumeg/base/pipelines/jumeg_pipelines_utils1.py
+++ jumeg/base/pipelines/jumeg_pipelines_utils1.py
@@ -39,7 +39,7 @@ from jumeg.jumeg_interpolate_bads  import interpolate_bads as jumeg_interpolate_
 
 logger = logging.getLogger("jumeg")
 
-__version__= "2019.05.16.001"
+__version__= "2019.05.24.001"
 
 
 #--- init Class JuMEG_PipelineFrame as instance
@@ -175,270 +175,35 @@ def apply_interpolate_bads(raw_fname=None,raw=None,config=None,label="interpolat
 
     return fname_out,raw,True
 
-
-#---------------------------------------------------
-#--- apply_noise_reducer
-#---------------------------------------------------
-def _apply_noise_reducer(raw_fname,raw=None,config=None):
-    '''
-    apply <magic ee noise reducer> thrice to reference channels with different freq parameters
-    save PSD plot in subfolder /plots
-
-    !!! overwrite raw-obj, works inplace !!!
-
-    0) reset bads and check for dead channels
-    1) apply nr low pass filter for freq below e.g.: 5Hz  <reflp>
-    2) apply nr high pass filter if defined               <reflp>
-    3) apply nr notch filter to remove power line noise   <refnotch>
-    4) save PSD plot
-
-    Parameter:
-    -----------
-    <noise_reducer> parameter used in this function :
-     fname_raw  : input raw filename
-     raw        : <None>\n
-     cfg        : dict, part of config file <None>
-        from config file <noise_reducer> part\n
-        reflp      : <None>\n
-        refhp      : <None>\n
-        refnotch   : <None>\n
-
-        plot: True
-        plot_show : True
-        plot_dir   : subdir to save plots
-
-        postfix       : "nr"
-        file_extention: ["meeg-raw.fif","rfDC-empty.fif"]
-
-        run      : True
-        save     : True
-        overwrite: True
-
-    ToDo add parameter extended
-    <noise_reducer> parameter extended
-       signals=[], noiseref=[], detrending=None,
-       tmin=None, tmax=None,
-       exclude_artifacts=True, checkresults=True, return_raw=False,
-       complementary_signal=False, fnout=None, verbose=False
-
-    Return:
-    --------
-     filename,raw-obj
-    '''
-    with JuMEG_PipelineFrame(raw_fname=raw_fname,raw=raw,name="noise reducer",config=cfg) as JPF:
-        if not JPF.run or not JPF.raw:
-            return JPF.fname_out,JPF.raw
-        
-        #--- check dead channes and mark them as bad
-        jb.picks.check_dead_channels(raw=JPF.raw)
-        
-        #--- start plot denoising orig raw psd, avoid reloading raw data
-        if config.get("plot"):
-            jplt = JuMEG_PLOT_PSD(n_plots=2,name="denoising",verbose=True)
-            jplt.plot(JPF.raw,title="orig: " + os.path.basename(JPF.raw_fname),check_dead_channels=False,
-                      fmax=config.get("fmax"))
-        
-        #--- with redirect stdout/err
-        with jumeg_logger.StreamLoggerSTD(label="noise_reducer"):
-            #--- 1 nr low pass filter for freq below 5 hz
-            if config.get("reflp"):
-                JPF.raw = noise_reducer(None,raw=JPF.raw,reflp=config.get("reflp"),return_raw=True,
-                                        verbose=config.get("verbose"),exclude_artifacts=False)
-                JPF.RawIsChanged = True
-            #--- 2 nr high pass filter
-            if config.get("refhp"):
-                JPF.raw = noise_reducer(None,raw=JPF.raw,reflp=config.get("refhp"),return_raw=True,
-                                        verbose=config.get("verbose"),exclude_artifacts=False)
-                JPF.RawIsChanged = True
-            #--- 3  nr notch filter to remove power line noise
-            if config.get("refnotch"):
-                JPF.raw = noise_reducer(None,raw=JPF.raw,refnotch=config.get("refnotch"),fnout=None,return_raw=True,
-                                        verbose=config.get("verbose"),exclude_artifacts=False)
-                JPF.RawIsChanged = True
-        
-        #--- plot results, avoid reloading raw data
-        if config.get("plot"):
-            jplt.plot(JPF.raw,title="denoised: " + os.path.basename(JPF.fname_out),check_dead_channels=False,
-                      fmax=config.get("fmax"))
-            if config.get("plot_show"):
-                jplt.show()
-            jplt.save(fname=JPF.fname_out,plot_dir=config.get("plor_dir","plots"))
-
-
-#---------------------------------------------------
-#--- apply_suggest_bads
-#---------------------------------------------------
-#@JuMEG_PipelineFrame
-def _apply_suggest_bads(raw_fname,raw=None,config=None,name="suggest_bads"):
-    """
-
-    :param raw_fname:
-    :param raw:
-    :param cfg:
-    :return:
-     filename,raw-obj
-    """
-    return
-    
-    JPF = JuMEG_PipelineFrame(raw_fname=raw_fname,raw=raw,name="noise reducer",config=cfg)
-    with JPF.excecute():
-         if not JPF.run or not JPF.raw:
-            return JPF.fname_out,JPF.raw
-      
-         with jumeg_logger.StreamLoggerSTD(label="suggest_bads"):
-              marked,raw = suggest_bads(raw) #,**cfg["parameter"]) #show_raw=config.get("show_raw") )
-         
-             #--- set bads in global HDF
-              fhdf = os.path.join( config.get("stage"),config.get("hdfname","badchannels.hdf5"))
-              update_bads_in_hdf(fhdf=fhdf,bads=marked,fname=raw_fname,verbose=config.get("verbose"))
-    
-    
-    #fname_out,raw = jb.update_and_save_raw(raw,fin=JPF.raw_fname,fout=None,save=config.get("save"),update_raw_filenname=True,
-    #                                       postfix=config.get("postfix","bcc"),overwrite=config.get("overwrite",True))
- 
-    if fname_out:
-       return fname_out,raw
-    else:
-       raise Exception( "---> ERROR file name not defined !!!" )
-      
-
-#---------------------------------------------------
-#--- apply_interpolate_bads
-#---------------------------------------------------
-def _apply_interpolate_bads(raw_fname,raw=None,config=None):
-    """
- 
-    :param raw_fname:
-    :param raw:
-    :param cfg:
-    :return:
-     filename,raw-obj
-    """
-    return
-    
-    fname_out = None
-    logger.info("  -> apply_interpolate_bad file name: {}".format(raw_fname))
-    logger.debug("  -> config parameter:\n{}".format(cfg))
-    jb.verbose = config.get("verbose")
-    
-    if not jb.check_file_extention(fname=raw_fname,file_extention=config.get("file_extention")):
-       return
-    #--- return raw_fname,raw
-    if not config.get("run"):
-       return jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=False,update_raw_filenname=True,postfix=config.get("postfix","int"),overwrite=config.get("overwrite",True))
-
-    raw,raw_fname = jb.get_raw_obj(raw_fname,raw=raw)
-    
-    if raw:
-       logger.info("fname: {}".format(raw_fname) )
-      #--- Interpolate bad channels using jumeg
-       with jumeg_logger.StreamLoggerSTD(label="interpolate_bads"):
-            raw = jumeg_interpolate_bads(raw) #,**config.get("parameter"))  #,origin=config.get("origin",None),reset_bads=config.get("reset_bads",True) )
-    
-    #-- check results
-       if config.get("plot_block"):
-          raw.plot(block=config.get("plot_block"))
-    
-    #--- update filename in raw and save if save
-    fname_out,raw = jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=config.get("save"),
-                                           postfix=config.get("postfix","int"),overwrite=config.get("overwrite",True))
-   
-    if fname_out:
-       return fname_out,raw
-    else:
-       raise Exception( "---> ERROR file name not defined !!!" )
-       
-    
-
 #---------------------------------------------------
 #--- apply_filter
 #---------------------------------------------------
-def apply_filter(raw_fname,raw=None,config=None):
+@JuMEG_PipelineFrame
+def apply_filter(raw_fname,raw=None,config=None,label="filter",fname_out=None):
     """
-
+    ToDo implement call to mne filter
     :param raw_fname:
     :param raw:
     :param cfg:
     :return:
-     filename,raw-obj
+     filename,raw-obj,True
     """
-    return
-    logger.info("  -> apply_filter file name: {}".format(raw_fname))
-    logger.debug("  -> config parameter:\n{}".format(cfg))
-
-    if not jb.check_file_extention(fname=raw_fname,file_extention=config.get("file_extention")):
-        return
-        #--- return raw_fname,raw
-    if not config.get("run"):
-        return jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=False,
-                                      postfix=config.get("postfix","fi"),overwrite=config.get("overwrite",True))
-
-    #--- catch stdout,stderr
-    #jumeg_logger.log_stdout(label="filter")
-    #jumeg_logger.log_stderr(label="filter")
-
-    jb.verbose = config.get("verbose")
-    raw,raw_fname = jb.get_raw_obj(raw_fname,raw=raw)
     
-    #--- ToDo setup  mne filter as jumeg CLS
-    #raw,raw_fname = jumeg_mne_fileter(raw)
-    raw_changed=True
-    
-    #--- save and update filename in raw
-    #if raw_changed:
-    #    fname_out,raw = jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=config.get("save"),update_raw_filenname=True,
-    #                                           postfix=config.get("postfix","bcc"),overwrite=config.get("overwrite",True))
-
-   #--- return back stdout/stderr from logger
-    #jumeg_logger.log_stdout(reset=True)
-    #jumeg_logger.log_stderr(reset=True)
-
-    return fname_out,raw
-
+    return fname_out,raw,True
 
 #---------------------------------------------------
 #--- apply_resample
 #---------------------------------------------------
-def apply_resample(raw_fname,raw=None,config=None):
+@JuMEG_PipelineFrame
+def apply_resample(raw_fname,raw=None,config=None,label="resample",fname_out=None):
     """
-
+    ToDo implement call to mne resample
     :param raw_fname:
     :param raw:
     :param cfg:
     :return:
      filename,raw-obj
     """
-    return
-    logger.info("  -> apply_resample file name: {}".format(raw_fname))
-    logger.debug("  -> config parameter:\n{}".format(cfg))
-    
-    if not jb.check_file_extention(fname=raw_fname,file_extention=config.get("file_extention")):
-       return
-        #--- return raw_fname,raw
-    if not config.get("run"):
-       return jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=False,
-                                      postfix=config.get("postfix","res"),overwrite=config.get("overwrite",True))
-
-   #--- catch stdout,stderr
-    #jumeg_logger.log_stdout(label="filter")
-    #jumeg_logger.log_stderr(label="filter")
-
-    jb.verbose = config.get("verbose")
-    raw,raw_fname = jb.get_raw_obj(raw_fname,raw=raw)
-    
-    #--- ToDo setup  resampling
-    #raw,raw_fname = jumeg_mne_fileter(raw)
-    raw_changed=True
-    
-    #--- save and update filename in raw
-    #if raw_changed:
-    #    fname_out,raw = jb.update_and_save_raw(raw,fin=raw_fname,fout=None,save=config.get("save"),update_raw_filenname=True,
-    #                                          postfix=config.get("postfix","bcc"),overwrite=config.get("overwrite",True))
-
-   #--- return back stdout/stderr from logger
-    #jumeg_logger.log_stdout(reset=True)
-    #jumeg_logger.log_stderr(reset=True)
-
-    return fname_out,raw
+    return fname_out,raw,True
 
 
